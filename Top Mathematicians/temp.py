b6322d12b452e4f37f4e600f6f31648bbbcf019a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-15 20:25:22 +0600

    web scraping

--- Top Mathematicians/temp.py
+++
@@ -1,7 +0,0 @@
-def is_good_response(resp):
-    
-    #Return True if the response seems to be HTML, false otherwise
-     
-    content_type = resp.headers["content_type"].lower() 
-    
-    return (resp.status_code == 200 and content_type is not None and content_type.find("html"))
