0eb05c7dafa27dc213dc7e2674174d7a3b592da6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-16 03:39:48 +0600

    web scraping

--- scrapingtest_2.py
+++ scrapingtest_2.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
-
+from urllib.error import URLError
 
 try:
     html = urlopen("http://www.pythonscraping.com/pages/page1.html")
