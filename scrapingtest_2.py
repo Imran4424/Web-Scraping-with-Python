d55c0971cd84c65224c3c7b89c73b6a628f5c750
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-16 03:32:40 +0600

    web scraping

--- scrapingtest_2.py
+++ scrapingtest_2.py
@@ -1,10 +1,2 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-
-bsobj = BeautifulSoup(html.read(), 'lxml')
-
-#print(bsobj)
-print(bsobj.prettify)
-
+from urllib.error import HTTPError
