1661eaee348a9e4d153f6e92ad5bf1dc8dcfb178
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-15 20:16:11 +0600

    web scraping

--- Top Mathematicians/mathematicians2.py
+++ Top Mathematicians/mathematicians2.py
@@ -1,25 +1,6 @@
 from requests import get
-from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
-
-
-''' def simple_get(url):
-    """
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None
-    """
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None '''
 
 
 def simple_get(url):
@@ -40,41 +21,33 @@ def is_good_response(resp):
     """
     Returns true if the response seems to be HTML, false otherwise
     """
+
     content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200
-            and content_type is not None
-            and content_type.find('html') > -1)
-
-
-def log_error(e):
-    """
-    It is always a good idea to log errors. 
-    This function just prints them, but you can
-    make it do anything.
-    """
-    print(e)
+    return (resp.status_code == 200 and content_type is not None and content_type.find("html") > -1)
 
 
 def get_names():
-    """
-    Downloads the page where the list of mathematicians is found
-    and returns a list of strings, one per mathematician
-    """
-    url = 'http://www.fabpedigree.com/james/mathmen.htm'
+    '''
+    downloads the the page where list of mathematicians is found and return a list of strings, one per mathematician
+    '''
+
+    url = "http://www.fabpedigree.com/james/mathmen.htm"
+
     response = simple_get(url)
 
     if response is not None:
-        html = BeautifulSoup(response, 'html.parser')
-        names = set()
-        for li in html.select('li'):
-            for name in li.text.split('\n'):
-                if len(name) > 0:
-                    names.add(name.strip())
+        html = BeautifulSoup(response, "html.parser")
+        #names = set()
+        #for li in html.select("li"):
+        #for name in li.text.split("\n"):
+        #if len(name) > 0:
+        #names.add(name.strip())
         #return list(names)
-        print(names)
+        #print(names)
 
-    # Raise an exception if we failed to get any data from the url
-    raise Exception('Error retrieving contents at {}'.format(url))
+        # here li is a tag name, so must be li
+        for i, li in enumerate(html.select("li")):
+            print(i+1, li.text)
 
 
 get_names()
