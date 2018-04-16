cd6881384f51502e34440cc3657f4429ab621668
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-16 12:27:42 +0600

    web scraping

--- Educational Board Result/EducationalBoard.py
+++ Educational Board Result/EducationalBoard.py
@@ -13,3 +13,4 @@ capcha = eval(form.form.table.table.find_all("tr")[6].find_all("td")[1].get_text
 
 
 data = dict(sr=3,et=0,exam="ssc",year="2012",board = "dinajpur",roll="138272",reg = "750336",value_s = capcha)
+
