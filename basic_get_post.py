b6a09ddaa00d433915f79f42bebc7c4cc1177fed
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-16 07:36:23 +0600

    web scraping

--- basic_get_post.py
+++ basic_get_post.py
@@ -4,7 +4,8 @@ import bs4
 url = "http://www.educationboardresults.gov.bd/"
 
 res1 = requests.get(url)
-soup1 = bs4.BeautifulSoup(res1.content, 'lxml')
+soup1 = bs4.BeautifulSoup(res1.content, "lxml")
 x1 = soup1.find_all()
 x1.attrs
-x1['value']
+
+print(x1)
