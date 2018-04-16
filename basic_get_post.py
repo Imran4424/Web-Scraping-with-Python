2e3f2656668e3604b24c609dcff63c8a3dbb04f0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-16 07:34:52 +0600

    web scraping

--- basic_get_post.py
+++ basic_get_post.py
@@ -5,6 +5,6 @@ url = "http://www.educationboardresults.gov.bd/"
 
 res1 = requests.get(url)
 soup1 = bs4.BeautifulSoup(res1.content, 'lxml')
-x1 = soup1.find('input', {'name': 'clear_search'})
+x1 = soup1.find_all()
 x1.attrs
 x1['value']
