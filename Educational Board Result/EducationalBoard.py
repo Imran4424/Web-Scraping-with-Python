7960b1c7b191df53d91a4281d6132909e50d81ba
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-16 12:25:58 +0600

    web scraping

--- Educational Board Result/EducationalBoard.py
+++ Educational Board Result/EducationalBoard.py
@@ -10,3 +10,6 @@ main_page = session.get(url)
 form = BeautifulSoup(main_page.content,"lxml")
 
 capcha = eval(form.form.table.table.find_all("tr")[6].find_all("td")[1].get_text())
+
+
+data = dict(sr=3,et=0,exam="ssc",year="2012",board = "dinajpur",)
